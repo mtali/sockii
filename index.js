19a0b1c5ac47d1717e2ea20b7b0c54256d20d588
mtali <emmanuelmtali@yahoo.com>
2022-06-06 18:23:12 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const { instrument } = require("@socket.io/admin-ui");
+
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true
+  }
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('publish', (msg) => {
+        console.log('Publish message: ' + msg);
+    });
+
+});
+
+instrument(io, {
+  auth: false
+});
+
+httpServer.listen(3000);
